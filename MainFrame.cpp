99e660442ba48a1ac2ce26f1c71f538f487e70e3
congtranv <congtranv@outlook.com>
2024-01-22 21:56:46 +0700

    Mouse click events

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -3,31 +3,15 @@
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button 1", wxPoint(300, 275), wxSize(200, 50));
-	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 325), wxSize(200, 50));
+	CreateStatusBar();
 
-	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClosed, this);
-	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
-	button1->Bind(wxEVT_BUTTON, &MainFrame::On1stButtonClicked, this);
-	button2->Bind(wxEVT_BUTTON, &MainFrame::On2ndButtonClicked, this);
-
-	CreateStatusBar(); 
+	// wxEVT_LEFT_DOWN | wxEVT_RIGHT_DOWN | wxEVT_MIDDLE_DOWN - mouse click
+	// wxEVT_LEFT_DCLICK - double click
+	panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseEvent, this);
 }
 
-void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt) {
-	wxLogMessage("Button clicked");
-}
-
-void MainFrame::On1stButtonClicked(wxCommandEvent& evt) {
-	wxLogStatus("Button 1 clicked");
-	evt.Skip();
-}
-void MainFrame::On2ndButtonClicked(wxCommandEvent& evt) {
-	wxLogStatus("Button 2 clicked");
-	evt.Skip();
-}
-
-void MainFrame::OnClosed(wxCloseEvent& evt) {
-	wxLogMessage("Window closed");
-	evt.Skip();
+void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
+	wxPoint mousePos = evt.GetPosition();
+	wxString message = wxString::Format("Mouse Event Detected (x = %d, y = %d)", mousePos.x, mousePos.y);
+	wxLogStatus(message);
 }
--- MainFrame.h
+++ MainFrame.h
@@ -6,8 +6,5 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnAnyButtonClicked(wxCommandEvent& evt);
-	void On1stButtonClicked(wxCommandEvent& evt);
-	void On2ndButtonClicked(wxCommandEvent& evt);
-	void OnClosed(wxCloseEvent& evt);
+	void OnMouseEvent(wxMouseEvent& evt);
 };
